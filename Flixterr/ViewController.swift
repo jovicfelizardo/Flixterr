7b22e48d0291e08114e16348c94e2dea7819b73c
Jiahan Li <jiahanlijiang99@gmail.com>
2019-10-19 13:40:01 -0400

    Initial Commit

---
+++ Flixterr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flixterr
+//
+//  Created by Jiahan Li on 10/19/19.
+//  Copyright © 2019 Jiahan Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
